b118a95d9a1641c079822fa51db536504771f6b3
k.kamkina <k.kamkina@vozovoz.ru>
2022-06-30 19:43:21 +0300

    add day 6

--- src/main/java/day6/Task3.java
+++ src/main/java/day6/Task3.java
@@ -1,16 +1,37 @@
 package day6;
+import java.util.Random;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        Teacher teacher1 = new Teacher();
+        Student student1 = new Student();
+        teacher1.setName("Mr. Life");
+        teacher1.setSubject("Life science");
+        student1.setName("Kamkina Karina Aleksandrovna");
+        teacher1.setEvaluate(student1.name);
+        teacher1.getEvaluate();
     }
-
 }
 
-class Teacher{
+class Student{
+    static String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Teacher extends Student {
+    Student student = new Student();
+
     String name;
     String subject;
     int evaluate;
+    private Teacher student1;
 
     public void setName(String name) {
         this.name = name;
@@ -28,17 +49,15 @@ class Teacher{
         return subject;
     }
 
-//
-}
-class Student{
-    String name;
-
-    public void setName(String name) {
-        this.name = name;
+    public void setEvaluate(String name) {
+        Random random = new Random();
+        this.evaluate = random.nextInt((5 - 3) + 1) + 3;
     }
 
-    public String getName() {
-        return name;
+    public int getEvaluate() {
+        System.out.println("Преподаватель "+name+" оценил студента с именем "+Student.name+" по предмету "+subject+" на оценку "+evaluate+".");
+        return evaluate;
+
     }
 }
 
