c404ad17dbeeb9a5f19d60c14ab7d2bbf8637a20
kakamkina <karinakamkina@gmail.com>
2022-07-17 14:27:33 +0300

    add day 8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,20 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+        long startTime = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder(1 + " ");
+        for (int a = 2; a < 20001; a++) {
+            sb.append(+a + " ");
+        }
+        System.out.println(sb);
+        long stopTime = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTime - startTime));
+
+        long startTime1 = System.currentTimeMillis();
+        for (int a = 0; a < 20000; a++) {
+            System.out.print(a + " ");
+        }
+        long stopTime1 = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTime1 - startTime1));
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -3,5 +3,63 @@ package day8;
 public class Task2 {
     public static void main(String[] args) {
 
+        Airplane airplane = new Airplane("Boeing", 2000, 150, 10000);
+        System.out.println(airplane);
+
+    }
+}
+
+class Airplane {
+
+    String manufacturer;
+    int year;
+    int length;
+    int weight;
+    int fuel;
+    int fillUp;
+
+    public Airplane(String manufacturer, int year, int length, int weight) {
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+        fuel = 0;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public void setFillUp(int n) {
+        this.fillUp = fillUp;
+        fillUp = fillUp + n;
+    }
+
+    public int getFillUp() {
+        return fillUp;
+    }
+
+    public String toString() {
+        return manufacturer + " " + year + " " + length + " " + weight;
     }
 }
